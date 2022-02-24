a55815562017661736bdb992862238d2321a6b86
mgoldp <mgoldp2013@gmail.com>
2022-02-24 17:07:45 -0300

    Comentarios en dart

---
+++ 01_basico/04_comentarios.dart
@@ -0,0 +1,30 @@
+/** 
+ * Este archivo fue creado para demostra comentarios en dart
+ *  [creadoEn] : 24-02-2022
+ * 
+*/
+
+main() {
+  // Esta es una variable temporal que quiero definir
+  int a = 10; // a debería ser 10
+
+  /*
+  final personas = [
+    // Esta es la primera persona
+    'Fernando', // [0]
+
+    // Esta es la segunda persona
+    'Pedro', // [1]
+
+    // Esta es la tercera persona
+    'Carlos', // [2]
+  ];
+  */
+}
+
+// Nombre: El nombre de la persona que quiero saludar
+// Mensaje: El saludo que suiero hacer
+
+/// Función para saludar
+///  Recibe un [nombre] y se concatena con el [mesaje]
+saludar(int nombre, int mensaje) {}
