52bc9a0067cfa11968f61095f0d3e4e6b696d0a6
mgoldp <mgoldp2013@gmail.com>
2022-02-24 18:05:21 -0300

    operadores de asignación y condicionales

---
+++ 01_basico/06_operadores.dart
@@ -0,0 +1,19 @@
+main() {
+  // Operadores de asignación
+
+  int a = 10; // = asigna valor a la variable
+  int b; //= 1;
+
+  //b ??= 20; // Asignar el valor unicamente si la variable es null
+
+  //print(b);
+
+  // Operadores condicionales
+  int c = 28;
+  String resp = c > 25 ? 'C es mayor a 25' : 'C es menor a 25';
+
+  //print(resp);
+
+  int d = b ?? a;
+  print(d);
+}
