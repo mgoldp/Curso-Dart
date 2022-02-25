7f6c46fb926fdf01bebf42efb23da42d20dd4a5d
mgoldp <mgoldp2013@gmail.com>
2022-02-25 12:51:29 -0300

    Se agrega carpeta de control y se importa dart:io

---
+++ 02_control/01_obtener_input.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+main() {
+  // Imprimir en la terminal o cmd
+  stdout.writeln('¿Cuál es tu nombre?');
+
+  // Leer información
+  String? nombre = stdin.readLineSync();
+
+  stdout.writeln('Tu nombre es : $nombre');
+}
