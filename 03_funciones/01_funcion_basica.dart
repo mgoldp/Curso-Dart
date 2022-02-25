24ff65ab11025ebf175c8266126f9412d08bcf1b
mgoldp <mgoldp2013@gmail.com>
2022-02-25 20:45:03 -0300

    funcion básica

---
+++ 03_funciones/01_funcion_basica.dart
@@ -0,0 +1,15 @@
+void main() {
+  var mensaje = saludar();
+  print(mensaje);
+}
+
+// void saludar() {
+//   print('Hola Fernando');
+//   print('Hola Maria');
+
+//   return 0;
+// }
+
+String saludar() {
+  return 'Hola Fernando';
+}
