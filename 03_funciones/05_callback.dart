5d3bc6c3d38ce7918492e7ad6419e89994194e8e
mgoldp <mgoldp2013@gmail.com>
2022-02-27 21:13:39 -0300

    callback

---
+++ 03_funciones/05_callback.dart
@@ -0,0 +1,10 @@
+void main() {
+  obtenerUsuario('100', (Map persona) {
+    print(persona);
+  });
+}
+
+void obtenerUsuario(String id, Function callback) {
+  Map usuario = {'id': id, 'nombre': 'Juan Carlos'};
+  callback(usuario);
+}
