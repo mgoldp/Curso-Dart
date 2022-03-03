66bebe8b15ef276f92ed76a41dea602cf06352c9
mgoldp <mgoldp2013@gmail.com>
2022-03-03 17:31:58 -0300

    Más propiedades sobre doubles

--- 07_docs/01_double.dart
+++ 07_docs/01_double.dart
@@ -1,5 +1,12 @@
 void main(List<String> args) {
   double numero = 3.1416;
+  double infinito = double.infinity;
 
-  print('Firma: ${numero.sign} :: $numero');
+  //print('Firma: ${numero.sign} :: $numero');
+  //más funciones nagtivas (core) sobre doubles
+  print('Si es finito: ${numero.isFinite} :: $numero');
+  print('Si es finito: ${infinito.isFinite} :: $numero');
+  print('abs: ${numero.abs()} :: $numero');
+  print('ceil: ${numero.ceil()} :: $numero');
+  print('ceilToDouble: ${numero.ceilToDouble()} :: $numero');
 }
