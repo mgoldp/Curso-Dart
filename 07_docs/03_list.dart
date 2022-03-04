c1953a22cfc995821785382708d4b1a328ffcb8a
mgoldp <mgoldp2013@gmail.com>
2022-03-04 17:40:00 -0300

    Más métodos de listas

--- 07_docs/03_list.dart
+++ 07_docs/03_list.dart
@@ -15,4 +15,34 @@ void main(List<String> args) {
   print('asMap: ${lista.asMap()[2]} ');
   Map listaMapa = lista.asMap();
   print('ListaMapa: ${listaMapa[4]} ');
+  Map nombreMapa = nombre.asMap();
+  print('NombreMapa: ${nombreMapa} ');
+  print('indexOf: ${nombre.indexOf('Peter')} ');
+  // int mayor3 = lista.indexWhere((numero) {
+  //   if (numero > 3) {
+  //     return true;
+  //   } else
+  //     return false;
+  // });
+  int mayor3 = lista.indexWhere((numero) => (numero > 3) ? true : false);
+  print('indexWhere mayor que 3: $mayor3');
+
+  print('Remove: ${nombre.remove('Tony')} ');
+  print('Remove: ${nombre} ');
+
+  lista.shuffle();
+  print('Shufle: $lista ');
+
+  lista3.sort();
+  print('Sort: $lista3');
+  print('Reverse: ${lista3.reversed.toList()} ');
+
+  nombre.forEach((nombre) {
+    nombre = nombre.toUpperCase();
+    print(nombre);
+  });
+  print('Listado: $nombre');
+
+  final newList = nombre.map((nombre) => nombre.toUpperCase()).toList();
+  print('New List: $newList');
 }
